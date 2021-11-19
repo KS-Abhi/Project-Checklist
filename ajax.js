75b0f6db5147b969ae0f7340de9d5b9b28288cbe
KS-Abhi <93191466+KS-Abhi@users.noreply.github.com>
2021-11-19 14:12:36 +0530

    Change

--- ajax.js
+++ ajax.js
@@ -1,4 +1,4 @@
-function item(){
+
     var xhttp = new XMLHttpRequest();  //creating XHR object
 
     // Event listener
@@ -6,31 +6,35 @@ function item(){
         if(this.readyState==4&&this.status==200){
             var response = JSON.parse(this.responseText);
             var list = "";
-            list = <tr>
+            list = `<tr>
             <th scope="col">Sl.No.</th>
+            <th>Status</th>
             <th scope="col">Item</th>
             <th scope="col">Qty</th>
             <th scope="col">Unit</th>
             <th scope="col">Dept.</th>
             <th scope="col">Notes</th>
-            </tr>;
+            </tr>`;
 
-            var Jshop = response.shopping;
+            var Jshop = response.items;
+            console.log(Jshop);
             var output = "";
             for (let i = 0; i <Jshop.length; i++) {
-                output += <tr>
-                <td class="sel1">${Jshop[i].Sl.No}</td>
-                <td class="sel3">${Jshop[i].Item}</td>
-                <td class="sel4">${Jshop[i].Qty}</td>
-                <td class="sel5">${Jshop[i].Unit}</td>
-                <td class="sel6">${Jshop[i].Dept}</td>
-                <td class="sel7">${Jshop[i].Notes}</td>
-                </tr>;
+                console.log(Jshop[i].it)
+                output += `<tr>
+                <td class="sel1">${Jshop[i].serialNumber}</td>
+                <td class="sel2"><input class="check" type="checkbox"></td>
+                <td class="sel3">${Jshop[i].item}</td>
+                <td class="sel4">${Jshop[i].quantity}</td>
+                <td class="sel5">${Jshop[i].unit}</td>
+                <td class="sel6">${Jshop[i].department}</td>
+                <td class="sel7">${Jshop[i].notes}</td>
+                </tr>`;
             }
             document.getElementById("heading").innerHTML = list;
             document.getElementById("listElements").innerHTML = output;
         }
     }
-}
+
     xhttp.open("GET", "data.json", true);
     xhttp.send();
